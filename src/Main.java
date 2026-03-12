2091fb6e28796229bf8ed3cd749b63b8b7189a39
andrejtihomirov <Andre0core@gmail.com>
2026-03-12 22:59:32 +0300

    completed hw 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+
 class Homework {
     public static void main(String[] args) {
         task1();
@@ -25,30 +29,35 @@ class Homework {
     public static void task1() {
         divideTask();
         System.out.println("task №1");
-        int year = 2028;
+        LocalDate date = LocalDate.of(2020, 3, 12);
+        int year = date.getYear();
         checkLeapYear(year);
         divideTask();
     }
 
 
     //task2
-    public static void chooseApp(int osType, int deviceYear) {
-        if (osType == 0 && deviceYear < 2015) {
+
+
+    public static void chooseApp(int osType, int year) {
+        if (osType == 0 && year < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (osType == 0 && deviceYear >= 2015) {
+        } else if (osType == 0 && year >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (osType == 1 && deviceYear < 2015) {
+        } else if (osType == 1 && year < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (osType == 1 && deviceYear >= 2015) {
+        } else if (osType == 1 && year >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
 
     public static void task2() {
+
         System.out.println("task №2");
-        int osType = 0;
-        int deviceYear = 2018;
-        chooseApp(osType, deviceYear);
+        LocalDate date = LocalDate.of(2026, 3, 12);
+        int year = date.getYear();
+        int osType = 1;
+        chooseApp(osType, year);
         divideTask();
     }
 
